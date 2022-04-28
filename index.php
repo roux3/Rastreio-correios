a1d05d4993151e3eb8e89666422611158f9aca7c
MrRouxx <43356551+roux3@users.noreply.github.com>
2022-04-28 16:52:17 -0300

    Update 2

--- index.php
+++ index.php
@@ -6,19 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <title>Rastreio</title>
 </head>
 <body>
+    
     <div class="menu">
-        <h1>Rastreio correios</h1>
+        <h1>Rastreio Correios</h1>
     </div>
     <div class="submenu">
         <ul>
             <li><a href="index.php">Rastrear</a></li>
-            <li><a href="salvos.php">Codigos</a></li>
+            <li><a href="salvos.php">Codigos
+            </a></li>
         </ul>
     </div>
+    <section class="corpo">
+        <div class="entrada">
+            <input class="texto" type="text" placeholder="Digite o código aqui...">
+            <button id="botao">Enviar</button>
+        </div>
+    </section>
+    <!--<script src="test.js"></script>!-->
 </body>
 </html>
 
-<?php echo "Ola" ?>
+<?php  ?>
---
+++ test.js
@@ -0,0 +1,4 @@
+btn = document.getElementById("botao")
+areaText = document.querySelector(".texto")
+btn.style.backgroundColor = "#007bff"
+
